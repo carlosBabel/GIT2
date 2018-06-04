f845c3a9ab7e95c8df34a9232779531371c46e44
carlosBabel <carlos.babel@gmail.com>
2018-06-04 11:20:13 +0200

    estilos personalizados

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Documento</title>
     <link rel="icon" type="image/png" href="./favicon.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
 
@@ -15,9 +16,11 @@
   <![endif]-->
 
     <?php include "menu.php"; ?>
-    <h2>Mi nuevo documento html</h2>
-    <p>Mi primer párrafo</p>
-    <p>Mi segundo párrafo</p>
+    <div class="container">
+        <h2>Mi nuevo documento html</h2>
+        <p>Mi primer párrafo</p>
+        <p>Mi segundo párrafo</p>
+    </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
